e4dc34ed267032d851e6c5c44949df3d1229ba4f
drill <coder78@hotmail.com>
2019-08-25 00:10:57 +0200

    Ajout du niveau 10

--- README.md
+++
@@ -1,2 +0,0 @@
-Download Bombjack Windows, MacOS et Linux
-https://sourceforge.net/projects/bombjack-remake/files/
--- Scripts/Level.gd
+++ Scripts/Level.gd
@@ -33,6 +33,7 @@ func setBotPos():
 		7: Constants.botInitPos = Vector2(190,200)
 		8: Constants.botInitPos = Vector2(156,120)
 		9: Constants.botInitPos = Vector2(170,120)
+		10: Constants.botInitPos = Vector2(160,160)
 
 func createBird():
 	var birdScene=preload('res://Scenes/Bird.tscn')
