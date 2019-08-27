c266550b33d53dd21fbdc7745e3229ef7d65ddd5
drill <coder78@hotmail.com>
2019-08-27 13:22:18 +0200

    Ajout du niveau  12

--- Scripts/Level.gd
+++ Scripts/Level.gd
@@ -35,7 +35,8 @@ func setBotPos():
 		9: Constants.botInitPos = Vector2(170,120)
 		10: Constants.botInitPos = Vector2(160,160)
 		11: Constants.botInitPos = Vector2(200,80)
-
+		12: Constants.botInitPos = Vector2(120,160)
+		
 func createBird():
 	var birdScene=preload('res://Scenes/Bird.tscn')
 	# Instancie l'oiseau 
